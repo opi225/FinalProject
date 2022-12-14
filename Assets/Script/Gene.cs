4d3b1e654fa94c17c62daf9cf0c5c7d30bc4c73f
opi225 <61334831+opi225@users.noreply.github.com>
2022-12-14 08:27:38 -0500

    stuff

--- Assets/Script/Gene.cs
+++ Assets/Script/Gene.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class Gene
 {
-    public string direction;
-    public int moveLength;
-    public int speed;
+    public float direction;
+    public float moveLength;
+    public float speed;
+
+    public Gene()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        direction = Random.Range(0.0f, 360.0f);
+        moveLength = Random.Range(0.0f, 1.0f);
+        speed = Random.Range(1.0f, 10.0f);
+    }
 }
